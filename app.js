995409550429c1b7833ce7b828950d2ea236e7a8
David Pfahler <pfahler.david@gmail.com>
2020-04-30 13:52:43 +0200

    changed port

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var app = express();
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+app.listen(80, () => {
+ console.log("Server running on port 80");
 });
 
 app.get("/url", (req, res, next) => {
