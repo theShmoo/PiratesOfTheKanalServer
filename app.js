2ad0196016883293b0187f1ec0c40386506532ac
David Pfahler <pfahler.david@gmail.com>
2020-04-30 14:49:45 +0200

    reverted to old app

--- app.js
+++ app.js
@@ -1,47 +1,28 @@
 var express = require("express");
-var path = require('path');
-var logger = require('morgan');
-
-var subscribe = require('./routes/subscribe');
-
 var app = express();
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// register the subscribe route
-app.use('/subscribe', subscribe);
-
-// catch 404 and forward to error handler
-app.use(logger('dev'));
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+var port = normalizePort(process.env.PORT || '3000');
+app.listen(port, () => {
+ console.log("Server running on port " + port);
 });
 
-// error handlers
+app.get("/", (req, res, next) => {
+ res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+});
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+/**
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+  return false;
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-module.exports = app;
