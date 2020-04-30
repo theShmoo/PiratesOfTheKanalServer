fd45358861f8796f13a4a4349296273e7b9ded9b
David Pfahler <pfahler.david@gmail.com>
2020-04-30 14:35:16 +0200

    added tests for plesk

--- app.js
+++ app.js
@@ -1,10 +1,47 @@
 var express = require("express");
+var path = require('path');
+var logger = require('morgan');
+
+var subscribe = require('./routes/subscribe');
+
 var app = express();
-var port = process.env.PORT
-app.listen(port, () => {
- console.log("Server running on port " + port);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// register the subscribe route
+app.use('/subscribe', subscribe);
+
+// catch 404 and forward to error handler
+app.use(logger('dev'));
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.get("/", (req, res, next) => {
- res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
+module.exports = app;
