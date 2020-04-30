d60f79ca9d39331cf822b778ed563a3d492d2b71
David Pfahler <pfahler.david@gmail.com>
2020-04-30 14:58:27 +0200

    getting post requests

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var express = require("express");
 var app = express();
 var port = normalizePort(process.env.PORT || '3000');
+
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.listen(port, () => {
  console.log("Server running on port " + port);
 });
 
-app.get("/", (req, res, next) => {
- res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+app.get("/subscribe", (req, res, next) => {
+ var mail = req.body["my-email"];
+ res.json({"mail": mail});
 });
 
 /**
