8a1a0333dabfdc1d0fd6a2ccd761b274ee4beb9f
David Pfahler <pfahler.david@gmail.com>
2020-04-30 16:16:32 +0200

    working mail subscribe server

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 var express = require("express");
+var cors = require('cors');
+var fs = require('fs');
+const { check, validationResult } = require('express-validator');
+
 var app = express();
+
 var port = normalizePort(process.env.PORT || '3000');
 
+// enable CORS
+app.use(cors())
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
  console.log("Server running on port " + port);
 });
 
-app.get("/subscribe", (req, res, next) => {
- var mail = req.body["my-email"];
- res.json({"mail": mail});
+app.post("/subscribe", [
+    check('mail').isEmail().normalizeEmail()
+  ], (req, res) => {
+
+  // Finds the validation errors in this request
+  // and wraps them in an object with handy functions
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  const mail = req.body.mail;
+  fs.appendFile('mails.txt', mail + "\n", (err) => {
+    if (err) throw err;
+    console.log('The mail ' + mail + ' was appended to file!');
+  });
+  res.sendStatus(200);
 });
 
 /**
