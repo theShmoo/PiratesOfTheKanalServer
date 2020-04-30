51c2324cc81c483afeb6f4312d6da75fa145c1d9
David Pfahler <pfahler.david@gmail.com>
2020-04-30 14:06:59 +0200

    fixed port to match with plex

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var app = express();
-
-app.listen(80, () => {
- console.log("Server running on port 80");
+var port = process.env.PORT
+app.listen(port, () => {
+ console.log("Server running on port " + port);
 });
 
 app.get("/", (req, res, next) => {
